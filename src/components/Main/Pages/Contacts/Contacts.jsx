3b735b69232911171034f38e0d693f3552a2f353
maneken <dozzadore@gmail.com>
2023-04-02 09:00:23 +0300

    customContacts

--- src/components/Main/Pages/Contacts/Contacts.jsx
+++ src/components/Main/Pages/Contacts/Contacts.jsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
 import "./contacts.css";
+import logoVk from "./images/logoVk.png";
+import logoWa from "./images/logoWa.png";
 
 
 const Contacts = () => {
@@ -6,12 +9,21 @@ const Contacts = () => {
         <div className="Contacts">
             <div className='contactsContainer'>
                 <div className="leftBoxContacts">
-                    <div>Whatsapp</div>
-                    <div>Phone</div>
+                    <div>
+                        <Link to="https://vk.com/elena_artnature">
+                            <img src={logoVk} alt='' className="leftBoxImg" />
+                            <div className="leftBoxVkP"><p>vk.com/elena_artnature</p></div>
+                        </Link>
+                    </div>
+                    <div>
+                        <img src={logoWa} alt='' className="leftBoxImg" />
+                        <p>Whatsapp</p>
+                    </div>
                     <div>Telegram</div>
-                    <div className="lefPContacts">Messengers</div>
                 </div>
-                <div className="midleBoxContacts"></div>
+                <div className="midleBoxContacts">
+                    <p>Какое-то супер описание того, что я принимаю заказы по построению лекал</p>
+                </div>
                 <div className="rightBoxContacts"></div>
             </div>
         </div>
